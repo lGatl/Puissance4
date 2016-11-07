a26beee8cd35e158983ffca5d34943af75ce7f07
lGATl <gat55@live.fr>
2016-11-07 19:18:54 +0100

    bon pour l'affichage des pions en haut

--- p4/p4.js
+++ p4/p4.js
@@ -1,1 +1,19 @@
 
+/*function myMove() {
+    var elem = document.getElementById("myAnimation"); 
+    var pos = 0;
+    var id = setInterval(frame, 10);
+    function frame() {
+        if (pos == 350) {
+            clearInterval(id);
+        } else {
+            pos++; 
+            elem.style.top = pos + 'px'; 
+            elem.style.left = pos + 'px'; 
+        }
+    }
+}*/
+/*
+element.onclick = function(e) { // L'argument « e » va récupérer une référence vers l'objet « Event »
+    alert(e.type); // Ceci affiche le type de l'événement (click, mouseover, etc.)
+}*/;
