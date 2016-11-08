105d323743a34085ecb23bfdf94d415f493864f3
lGATl <gat55@live.fr>
2016-11-08 01:24:48 +0100

    j ai voulu essayer de faire les animes sans jquerry, c'est trop la merde, il y a les animes css mais chaud d'etre responsive je vais vite faire une branche querry ^ ^

--- p4/p4.js
+++ p4/p4.js
@@ -13,7 +13,48 @@
         }
     }
 }*/
-/*
-element.onclick = function(e) { // L'argument « e » va récupérer une référence vers l'objet « Event »
-    alert(e.type); // Ceci affiche le type de l'événement (click, mouseover, etc.)
-}*/;
+
+anim("t",1)
+anim("t",2)
+anim("t",3)
+anim("t",4)
+anim("t",5)
+anim("t",6)
+anim("t",7)
+anim("t",8)
+
+function anim(t,n){
+
+
+		var element=document.getElementById(t+n);
+		element.onclick = function(e) {
+			
+			
+			elt= document.getElementById(element.id);
+			largbody=document.getElementById("body").offsetWidth;
+			topbody=document.getElementById("body").offsetTop;
+			taille1c=largbody/8;
+			lleft=taille1c*(n-1)
+			document.getElementById("jet0").id="jet"
+			document.getElementById("jet").style.left=lleft+8+"px";
+			document.getElementById("jet").style.top =20+ "px";
+			for(i=1;i<100;i++){
+				console.log("frtftrf");
+				setTimeout(function(){
+					console.log(i);i++
+					document.getElementById("jet").style.left=lleft+i+8+"px";
+
+				},1000)
+			}
+/*10
+*/		
+	     
+
+	   	 setTimeout(function(){document.getElementById("jet").id="jet0" },1000)
+						
+						}
+		
+	
+	
+}
+
