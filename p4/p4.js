8cf93fa5fffd61a2e3521eeb5e8726c71a867bd4
lGATl <gat55@live.fr>
2016-11-08 04:08:49 +0100

    Animation faite en jquerry

--- p4/p4.js
+++ p4/p4.js
@@ -1,18 +1,38 @@
 
-/*function myMove() {
-    var elem = document.getElementById("myAnimation"); 
-    var pos = 0;
-    var id = setInterval(frame, 10);
-    function frame() {
-        if (pos == 350) {
-            clearInterval(id);
-        } else {
-            pos++; 
-            elem.style.top = pos + 'px'; 
-            elem.style.left = pos + 'px'; 
-        }
-    }
-}*/
-/*
-element.onclick = function(e) { // L'argument « e » va récupérer une référence vers l'objet « Event »
-    alert
+/*ANIMER LE JETON*/
+
+$(".t").click(function(){
+/*	je met $("this en variable")*/ 
+thi=$(this)
+larbo=$(".body").width();
+haubo=$(".body").height();
+t=thi.position().top;
+l=thi.position().left;
+ col=Math.floor(l/(larbo/8))+1
+ lig=7
+ ta=$("#dc"+lig+col)
+$(".jeton").css({top: t+"px",left:l+"px"})
+
+jets(thi)
+$(".jeton").animate({top:t+(lig+1)*larbo/8},3000,function(){
+	
+	jeth(ta,thi);
+});
+
+});
+/*----------------------------*/
+/*AFFICHER JETON QUI BOUGE CACHER JETON STATIQUE*/
+function jets(t){
+	$(".jeton").show();
+	t.hide()
+}
+/*AFFICHER JETON statique CACHER JETON qui bouge*/
+function jeth(ta,t){
+	
+	$(".jeton").hide();
+	 
+ta[0].style.background="url('rouge.png') no-repeat"
+ta[0].style.backgroundSize="100%"
+	t.show()
+}
+/*----------------------------*/
